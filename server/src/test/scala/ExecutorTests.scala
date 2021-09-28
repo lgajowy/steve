fbad7ba2e5286919a4e0d4bd477e3720cb769456
Lukasz Gajowy <lukasz.gajowy@adverity.com>
2021-09-28 19:42:13 +0200

    Add an empty build test. Fiddle with the model.

---
+++ server/src/test/scala/ExecutorTests.scala
@@ -0,0 +1,15 @@
+package steve
+
+import munit.CatsEffectSuite
+import cats.Id
+
+class ExecutorTests extends CatsEffectSuite {
+  val exec = Executor.instance[Either[Throwable, *]]
+
+  test("Build empty image") {
+    assertEquals(
+      exec.build(Build.empty).flatMap(exec.run).map(_.getAll),
+      Right(Map.empty),
+    )
+  }
+}
--- shared/src/main/scala/steve/model.scala
+++ shared/src/main/scala/steve/model.scala
@@ -1,5 +1,12 @@
 package steve
 
+import cats.syntax.apply
+
+enum Command {
+  case Build(build: steve.Build)
+  case Run(hash: Hash)
+}
+
 case class Build(
   base: Build.Base,
   commands: List[Command],
@@ -9,14 +16,16 @@ object Build {
 
   enum Base {
     case EmptyImage
-    case ImageHash(hash: Hash)
+    case ImageReference(hash: Hash)
   }
 
-}
+  enum Command {
+    case Upsert(key: String, value: String)
+    case Delete(key: String)
+  }
 
-enum Command {
-  case Upsert(key: String, value: String)
-  case Delete(key: String)
+  val empty = Build(Base.EmptyImage, Nil)
+
 }
 
 case class Hash(value: Array[Byte])
